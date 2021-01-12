643ee7b706417a2fbae757d71ea0cc1ea6a3e224
Fiona Henry <fionahenry87@googlemail.com>
2021-01-12 17:37:10 +0000

    Fixed GradeProfile class

--- GradeProfile.cs
+++ GradeProfile.cs
@@ -6,14 +6,20 @@ namespace StudentReport
     class GradeProfile
     {
         public List<Module> moduleResults = new List<Module>();
-
-        public GradeProfile()
-        {
-            
-        }
+        public double moduleGrade;
+        
         public void CalculateAverageGrade()
         {
-            
+            foreach(Module result in moduleResults)
+            {
+                Console.WriteLine(result.moduleName);
+                foreach(Grade grade in result.studentGrades)
+                {
+                    moduleGrade+=(grade.markRecieved * grade.assignmentWeight);
+                    grade.DisplayGrade();
+                }
+                Console.WriteLine($"Overall module grade: {moduleGrade}");
+            }
         }
 
     }
